9b33bf5ff8f4ef7ed519391fa45d0d305b7137ef
Chenyu Wang <lvbenlvlv@gmail.com>
2025-04-25 12:54:39 +1000

    statement value is represented with -1, 0, 1

--- resolvers.py
+++ resolvers.py
@@ -1,15 +1,20 @@
 class TruthTables:
     def resolveAnd(self, statement1, statement2):
-        return statement1.value and statement2.value
+        if statement1.value != -1 and statement2 != -1:
+            return bool(statement1.value) and bool(statement2.value)
 
     def resolveOr(self, statement1, statement2):
-        return statement1.value or statement2.value
+        if statement1.value != -1 or statement2.value != -1:
+            return bool(statement1.value) or (statement2.value)
 
     def resolveNot(self, statement1):
-        return not statement1.value
+        if statement1.value != -1:
+            return not statement1.value
 
     def resolveImply(self, condition, consequence):
-        return (not condition.value) or consequence.value
+        if condition.value != -1 and consequence != -1:
+            return (not condition.value) or consequence.value
 
     def resolveIff(self, s1, s2):
-        return (s1.value or (not s2.value)) and ((not s1.value) or s2.value)
+        if s1.value != -1 and s2.value != -1:
+            return s1.value == s2.value
