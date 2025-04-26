3c2a4088360e55e89a56e093b473a83edbec67f5
Seanshen411 <seanshen41161@outlook.com>
2025-04-26 13:52:27 +1000

    Update stackmethod.py
    stack method with logit operations

--- stackmethod.py
+++ stackmethod.py
@@ -1,40 +1,45 @@
 from symbols import Connectives
 from resolvers import TruthTables
+from terms import Variable
 
 def logit_oper(operator, left, right=None):
+    truth_table = TruthTables()
     if operator == Connectives.AND:
-        return TruthTables.resolveAnd(left, right)
+        return truth_table.resolveAnd(left, right)
     elif operator == Connectives.OR:
-        return TruthTables.resolveOr(left, right)
+        return truth_table.resolveOr(left, right)
     elif operator == Connectives.NOT:
-        return TruthTables.resolveNot(left)
+        return truth_table.resolveNot(left)
     elif operator == Connectives.IMPLY:
-        return TruthTables.resolveImply(left, right)
+        return truth_table.resolveImply(left, right)
     elif operator == Connectives.IFF:
-        return TruthTables.resolveIff(left, right)
+        return truth_table.resolveIff(left, right)
 
-def evaluate_expression(expression):
+def evaluate_expression(expression): # return an variable object, access by .value
     stack = []
     for i in range(len(expression)):
         stack.append(expression[i])
         if expression[i] == ')':
             sub_expr = []
+            
             stack.pop() # Drop the last ')'
             while stack and stack[-1] != '(':
                 sub_expr.append(stack.pop())
             stack.pop()  # Remove the '('
             sub_expr = sub_expr[::-1]
-            
             result = evaluate_sub_expression(sub_expr)
-            stack.append(result)
+            
+            sub_statement_result = Variable("Sub_tmp")
+            sub_statement_result.set_value(result)
+            stack.append(sub_statement_result)
+            
     return stack[0] if stack else None
 
 def evaluate_sub_expression(sub_expr):
-    print(sub_expr)
     while len(sub_expr) > 1:
         left = sub_expr.pop(0)
         operator = sub_expr.pop(0)
         right = sub_expr.pop(0)
         result = logit_oper(operator, left, right)
         sub_expr.insert(0, result)
-    return sub_expr[0]
+    return result
